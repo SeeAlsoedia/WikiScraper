34572de1ba76c2997bd4752f21d609b1cc097414
miattorocha <miattorocha@hotmail.com>
2016-12-04 14:25:58 -0200

    Adding mongoose and mongoosastic to fill a elasticsearch database

--- server.js
+++ server.js
@@ -3,21 +3,51 @@ var request = require('request')
 var cheerio = require('cheerio')
 var htmlToText = require('html-to-text')
 var crypto = require('crypto')
+var mongoose = require('mongoose')
+var mongoosastic = require('mongoosastic')
 var listLinks = []
 
 var mongoClient = require('mongodb').MongoClient
 var mongoDbObj
 
 mongoClient.connect('mongodb://localhost:27017/WikiDb', function(err, db){
-  if(err)
-    console.log(err)
-  else {
-    console.log("Connected to MongoDB")
-    mongoDbObj = { db: db,
-      articles: db.collection('articles')
-    }
+	if(err)
+		console.log(err)
+	else {
+		console.log("Connected to MongoDB")
+		mongoDbObj = { 
+			db: db,
+			articles: db.collection('articles')
+		}
 	}
 })
+
+mongoose.connect('mongodb://localhost:27017/WikiDb')
+
+var articleSchema = new mongoose.Schema({
+	_id: {type: mongoose.Schema.Types.ObjectId, es_indexed:true },
+	title: {type: String, es_indexed:true },
+	url: String,
+	md5: String,
+	lastUpdated: Date,
+	raw: {type: String, es_indexed:true }
+})
+articleSchema.plugin(mongoosastic)
+
+var articleModel = mongoose.model("articles", articleSchema)
+
+articleModel.createMapping(function(err, mapping) {
+	if (err) {
+		console.log('error creating mapping')
+		console.log(err)
+	}
+	else {
+		console.log('mapping created')
+		console.log(mapping)
+	}
+})
+
+
 
 var urlOutdated = function(url, obj) {
 	var art = obj
@@ -29,20 +59,21 @@ var urlOutdated = function(url, obj) {
 			var article = data.shift()
 			if (article == null || article.md5 != art.md5) {
 				console.log("Upserting an Article")
+				if(article && article._id)
+					art._id = article._id
 				upsertArticle(art)
 			}
 		}
 	})
 } 
 
-var upsertArticle = function (json) {
-	var article = json
-	mongoDbObj.articles.save(json, {w:1}, function(err, result){
-		if(err){
-			console.log("Something go wrong. Error message: " + err)
-		}
+var upsertArticle = function (artSchema) {
+	var article = artSchema
+	articleModel.findOneAndUpdate({"url": article.url}, article, {upsert:true, new: true}, function(err, doc){
+		if(err)
+			console.log('upserting ERROR    :   ' + err)
 		else {
-			console.log("Upserted successfully. " + result)
+			console.log('upserting OK')
 		}
 	})
 }
@@ -61,13 +92,14 @@ var scrapeURL = function(url) {
 
 var requestUrl = function(url) {
 			var uri = url
+			var article = new articleModel()
 	        request(url, function(error, response, html){
             if(!error){
 				console.log("URL ACESSADA: " + uri)
+				article.url = uri;
                 var $ = cheerio.load(html)
 
                 var raw, md5
-                var json = { raw: "", title : "", url : uri, md5 : null, lastUpdated : null}
 				
                 $('#bodyContent').filter(function(){
                     var data = $(this)
@@ -79,18 +111,18 @@ var requestUrl = function(url) {
 					
 					md5 = crypto.createHash('md5').update(text).digest("hex") 
 
-                    json.raw = text
-                    json.md5 = md5
+                    article.raw = text
+                    article.md5 = md5
                 })
 
                 $('#firstHeading').filter(function(){
                     var data = $(this)
-                    json.title = data.text()
+                    article.title = data.text()
                 })
 
 
-                json.lastUpdated = new Date()
-				urlOutdated(json.url, json)
+                article.lastUpdated = new Date()
+				urlOutdated(article.url, article)
 				
                 if(listLinks.length > 0)
                     scrapeURL(listLinks.pop())
